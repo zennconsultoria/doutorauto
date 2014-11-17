0e17ec64425148c935d3d6353c74752ed3eb0d63
Cristovão <desenv@MacBook-Pro-de-Cristovao.local>
2014-11-17 12:50:03 -0200

    alteração layout código html

--- index.php
+++ index.php
@@ -19,18 +19,14 @@ elseif ($page_name=='index.html') {
 elseif ($page_name=='about.html') {
 	include $browser_t.'/about.html';
 	}
-elseif ($page_name=='service.html') {
-	include $browser_t.'/service.html';
+elseif ($page_name=='services.html') {
+	include $browser_t.'/services.html';
 	}
 elseif ($page_name=='blog.html') {
 	include $browser_t.'/blog.html';
 	}
 elseif ($page_name=='contact.html') {
 	include $browser_t.'/contact.html';
-	}
-elseif ($page_name=='contact-post.html') {
-	include $browser_t.'/contact.html';
-	include 'app/contact.php';
 	}
 else
 	{
