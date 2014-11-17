cabfb6d49805342bd2a9628f701c9cd227631a11
Cristovão <desenv@MacBook-Pro-de-Cristovao.local>
2014-11-17 14:22:47 -0200

    alteração layout código html

---
+++ web/index.php
@@ -0,0 +1,272 @@
+<html>
+<head>
+<title>DoutorAuto :: Home ::</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="web/css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link rel="stylesheet" href="web/css/slider.css" type="text/css" media="screen" />
+<script type="text/javascript" src="web/js/jquery-1.9.0.min.js"></script>
+<script type="text/javascript" src="web/js/jquery.nivo.slider.js"></script>
+<!----- Scroll top --------->
+<script type="text/javascript" src="web/js/move-top.js"></script>
+<script type="text/javascript" src="web/js/easing.js"></script>
+<!-----End  Scroll top --------->
+ <script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider();
+    });
+    </script>
+</head>
+<body>
+  <div class="header">  	
+		 <div class="header_top">
+		 	 <div class="wrap">
+		 			<div class="logo">
+		 				<a href="index.php"><img src="web/images/logo.png" alt="" /></a>
+					</div>
+					<div class="dail">
+						<h4>Entre em contato!</h4>
+						<p><img src="web/images/mobile.png">62 3922-0500</p>
+					</div>
+					<div class="clear"></div>
+					</div>	
+		     </div>
+		    
+		    <div class="header-bottom">
+	    	<div class="wrap">
+		     <div class="menu">
+					    <ul>
+							<li class="active"><a href="index.html">Home</a></li>
+							<li><a href="about.html">Sobre</a></li>
+							<li><a href="services.html">Serviços</a></li>
+							<li><a href="blog.html">Equipe</a></li>
+							<li><a href="contact.html">Contato</a></li>
+							<div class="clear"></div>
+						</ul>
+					</div>
+		        <div class="social-icons">						
+		                <ul>
+		                    <li><a class="facebook" href="<?=$link_facebook?>" target="_blank"> </a></li>
+		                    <li><a class="twitter" href="<?=$link_twitter?>" target="_blank"></a></li>
+		                    <li><a class="googleplus" href="<?=$link_google?>" target="_blank"></a></li>
+		                    <li><a class="pinterest" href="<?=$link_pin?>" target="_blank"></a></li>
+		                    <!--<li><a class="dribbble" href="#" target="_blank"></a></li>
+		                    <li><a class="vimeo" href="#" target="_blank"></a></li>-->
+		                    <div class="clear"></div>
+		                </ul>
+		 		    </div>
+		 		    <div class="clear"></div>
+		 		     </div>
+               </div>
+		  </div>				
+			    	<!--- Slider --->				
+     <div class="slider">
+      <div class="slider-wrapper theme-default">
+            <div id="slider" class="nivoSlider">
+                <img src="web/images/1.jpg" data-thumb="web/images/1.jpg" alt="" />
+                <img src="web/images/2.jpg" data-thumb="web/images/2.jpg" alt="" />
+                <img src="web/images/3.jpg" data-thumb="web/images/3.jpg" alt="" />
+                <img src="web/images/4.jpg" data-thumb="web/images/4.jpg" alt="" />
+            </div>
+        </div>
+   </div>
+			     <!--- End Slider --->
+		
+   <div class="wrap">
+   	 <div class="main">
+	    <div class="content">
+	    	<div class="content_top">
+	    	  <div class="section group">
+				<div class="col_1_of_4 span_1_of_4">
+					<div class="num-heading">
+				       <div class="number">
+							<figure><span>1</span></figure>
+					    </div>
+					       		<div class="heading">								
+									<h4>Mecânica</h4>
+							     </div>
+							 <div class="clear"></div>
+						</div> 
+						    <div class="heading-desc">	
+							     	<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
+					       		        <a href="#" class="button button-icon button-icon-demo">Read More</a>
+					       		</div>	
+				 </div>
+				<div class="col_1_of_4 span_1_of_4">
+					<div class="num-heading">
+				       <div class="number">
+							<figure><span>2</span></figure>
+					    </div>
+					       		<div class="heading">								
+									<h4>Teto Solar</h4>
+							     </div>
+							 <div class="clear"></div>
+						</div> 
+						    <div class="heading-desc">	
+							     	<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>					       		
+					       		<a href="#" class="button button-icon button-icon-demo">Read More</a>
+					       		</div>					
+				</div>
+				<div class="col_1_of_4 span_1_of_4">
+					<div class="num-heading">
+				       <div class="number">
+							<figure><span>3</span></figure>
+					    </div>
+					       		<div class="heading">								
+									<h4>Freios ABS</h4>
+							     </div>
+							 <div class="clear"></div>
+						</div> 
+						    <div class="heading-desc">	
+							     	<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
+					       		<a href="#" class="button button-icon button-icon-demo">Read More</a>
+					       		</div>					
+				</div>
+				<div class="col_1_of_4 span_1_of_4">
+					<div class="num-heading">
+				       <div class="number">
+							<figure><span>4</span></figure>
+					    </div>
+					       		<div class="heading">								
+									<h4>Air Bag</h4>
+							     </div>
+							 <div class="clear"></div>
+						</div> 
+						    <div class="heading-desc">	
+							     	<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
+					       		<a href="#" class="button button-icon button-icon-demo">Read More</a>
+					       		</div>
+				    </div>
+			   </div>
+	    	</div>	
+	    	   <div class="content-bottom">
+		    	   	<div class="section group">
+		    	   		<div class="col_1_of_3 span_1_of_3">
+						<h3>Seja Bem Vindo!</h3>
+						<img src="web/images/welcome.jpg" alt=""/>
+						<p style="padding-top:5px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					</div>
+						<div class="col_1_of_3 span_1_of_3">
+							<h3>O que fazemos</h3>
+							   <ul>
+		  						<li>Lorem ipsum dolor sit amet qui officia</li>
+		  						<li>Duis aute irure dolor in culpa qui</li>
+		  						<li>Sunt in culpa qui officia vel illum qui</li>
+		  						<li>vel illum qui dolorem eum wise man therefore</li>
+		  						<li>The wise man therefore in culpa qui officia</li>
+		  						<li>Sunt in culpa qui officia culpa qui officia</li>
+		  						<li>Lorem ipsum dolor sit amet qui officia</li>
+		  						<li>Duis aute irure dolor in culpa qui</li>
+		  						<li>Sunt in culpa qui officia vel illum qui</li>
+		  						<li>vel illum qui dolorem eum wise man therefore</li>
+		  						<li>The wise man therefore in culpa qui officia</li>
+		  						<li>Sunt in culpa qui officia culpa qui officia</li>
+		  			        </ul>
+						</div>					
+						<div class="col_1_of_3 span_1_of_3">
+							<h3>Nossa Equipe</h3>
+							<div class="post-grid">
+								<img src="web/images/team-1.jpg" alt="" />
+								<p><span>Amilson Monteiro</span></p>
+									<p>Lorem ipsum dolor sit amet consectetur dolor more normal of letters,<a href="#">[...]</a></p>
+								<div class="clear"> </div>
+					 		 </div>
+					 		 <div class="post-grid">
+								<img src="web/images/team-2.jpg"  alt="" />
+								<p><span>Douglas Freitas</span></p>
+									<p>Lorem ipsum dolor sit amet consectetur dolor more normal of letters,<a href="#">[...]</a></p>
+								<div class="clear"> </div>
+					 		 </div>
+					 		 <div class="post-grid">
+								<img src="web/images/team-3.jpg" alt="" />
+								<p><span>Evandro José</span></p>
+									<p>Lorem ipsum dolor sit amet consectetur dolor more normal of letters,<a href="#">[...]</a></p>
+								<div class="clear"> </div>
+					 		 </div>
+						</div>
+				  </div>
+			   </div>
+		    </div>
+		</div> 
+		</div>  
+   <div class="footer">
+   	 <div class="wrap">
+   	 <div class="footer_grides">
+	     <div class="section group">
+				<div class="col_1_of_4 span_1_of_4">
+					<h3>Últimas Notícias</h3>
+						<div class="post">
+				    		<p><span><a href="#">Tuesday,June 31th,2013</a></span></p>
+				    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,Ut enim ad minim veniam sed do <span><a href="#">[...]</a></span></p>
+				       </div>
+				       <div class="post">
+				    		<p><span><a href="#">Monday,May 21th,2013</a></span></p>
+				    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,Ut enim ad minim veniam sed do<span><a href="#">[...]</a></span></p>
+				       </div>
+				</div>
+				<div class="col_1_of_4 span_1_of_4">
+					<h3>Junte-se a nós</h3>
+					<div class="social_icons">
+                	<ul>
+                    	<li><a href="<?=$link_facebook?>" class="facebook">
+                        	<span class="icon"> &nbsp;</span> <span class="inner"><strong>Facebook</strong> <br>+ 12, 297</span>
+                        </a></li>
+                        <li><a href="<?=$link_twitter?>" class="twitter">
+                        	<span class="icon"> &nbsp;</span> <span class="inner"><strong>Twitter</strong> <br>+ 5, 287</span>
+                        </a></li>
+                         <li><a href="<?=$link_email?>" class="rss">
+                        	<span class="icon"> &nbsp;</span> <span class="inner"><strong>Rss</strong> <br>+ 77, 287</span>
+                        </a></li>
+                    </ul>
+                </div>
+				</div>
+				<div class="col_1_of_4 span_1_of_4">
+					<h3>Navegue</h3>
+					          <ul>
+						           <li><a href="index.html">Home</a></li>
+						     		<li><a href="about.html">Sobre</a></li>						     			
+						     		<li><a href="services.html">Serviços</a></li>
+						     		<li><a href="blog.html">Equipe</a></li>						     		
+						     		<li><a href="contact.html">Contato</a></li>
+						   	   </ul>
+				</div>
+				<div class="col_1_of_4 span_1_of_4">
+					<h3>Localização</h3>
+					      <ul>
+						  	  <li>Av. C-12 n°134 Qd. 66 Lt. 13</li>
+						  	  <li>Setor Sudoeste</li>
+						  	  <li>Goiânia - GO</li>
+						  	 <li><span>e-mail:</span> contato@doutorauto.com.br</li>
+						  	 <li><span>Telefone:</span> 62 3922 0500</li>
+						  </ul>
+				</div>
+			</div>
+	    </div>
+  </div>
+		 <div class="copy_right">
+		 	 <div class="wrap">
+				<p>DoutorAuto © Todos os Direitos Reservados | Design by  <a href="http://w3layouts.com">W3Layouts</a> | Desenvolvido por <a href="http://www.zenn.com.br">Zenn Consultoria</a></p>
+			 </div>
+		</div>	
+		<!------------ scroll Top ------------>
+	 <script type="text/javascript">
+		$(document).ready(function() {
+			/*
+			var defaults = {
+	  			containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+	 		};
+			*/
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	</script>
+    <a href="#" id="toTop"><span id="toTopHover"> </span></a>
+	<!------------ End scroll Top ------------>
+  </div>  
+</body>
+</html>
+
